cbd7638f4302dbde0f82f19649e1468b26ceb064
Louis Di Domenico <lou.didomenico@swiftkickweb.com>
2025-09-30 09:09:46 -0400

    Tailwind implementation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,9 @@ function About() {
     return (
         <div>
             <h1>About</h1>
+            <div className="bg-blue-500 text-white p-4 rounded-lg">
+                Hello Tailwind!
+            </div>
         </div>
     )
 }
