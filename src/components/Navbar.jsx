76764b472d595c3b78de9e474de96f41204f3475
Louis Di Domenico <lou.didomenico@swiftkickweb.com>
2025-10-27 16:16:01 -0400

    Making the mobile nav bar and menu visible. Some content updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,14 +30,14 @@ const Navbar = () => {
                 className={` ${open && "navbarTogglerActive"
                   } absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
               >
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-white dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-white dark:bg-white"></span>
+                <span className="relative my-[6px] block h-[2px] w-[30px] bg-white dark:bg-white"></span>
               </button>
               <nav
                 // :className="!navbarOpen && 'hidden' "
                 id="navbarCollapse"
-                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg text-white px-6 py-1 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${!open && "hidden"
+                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg text-white px-6 py-1 shadow-lg bg-blue dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none lg:dark:bg-transparent ${!open && "hidden"
                   } `}
               >
                 <ul className="block lg:flex">
