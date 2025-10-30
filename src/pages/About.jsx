08c02857d0f2c6a663f5393439e35f372f2f5688
Louis Di Domenico <lou.didomenico@swiftkickweb.com>
2025-10-30 13:36:06 -0400

    Fixing nav bar colors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
 function About() {
-    return (
-        <div>
-            <h1>About</h1>
-            <div className="bg-blue-500 text-white p-4 rounded-lg">
-                Hello Tailwind!
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>About</h1>
+      <div className="bg-blue-500 text-white p-4 rounded-lg">
+        Hello Tailwind!
+      </div>
+    </div>
+  )
 }
 
 export default About
